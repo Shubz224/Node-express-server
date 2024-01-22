72b1afcb6528234f9a6e34b584d2e256f93ac1e4
Shubham  Dharmaraj Nikam <nikamshubham224@gmail.com>
2024-01-22 21:34:14 +0530

    added url for push req

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
+const bodyParser = require("body-parser");
 const express = require("express");
 const path = require("path");
-const app =express();
+const app = express();
+const router = require("./UserRoutes");
+
 const port = 1000;
 
-
-app.get("/",(req,res)=>{
-    res.sendFile(path.join(__dirname + "/index.html"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use("/api/v1", router);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/index.html"));
+  
 });
 
-
-
-app.listen(port,()=>{
-    console.log(`server is working on port : ${port}`);
+app.listen(port, () => {
+  console.log(`server is working on port : ${port}`);
 });
